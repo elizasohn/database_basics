fdc936971bbc5f9d68da442b764594f067e55b5c
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-10 11:00:08 -0800

    add methods to volunteer and project class

--- lib/project.rb
+++ lib/project.rb
@@ -7,4 +7,51 @@ class Project
     @id = attributes.fetch(:id, nil)
   end
 
+  def self.all
+    returned_projects = DB.exec('SELECT * FROM projects;')
+    projects = []
+    returned_projects.each() do |project|
+      name = project.fetch('name')
+      id = project.fetch('id').to_i
+      projects.push(Project.new({:name => name, :id => id}))
+    end
+    projects
+  end
+
+  def self.search(x)
+    projects = Project.all
+    projects.select {|e| /#{x}/i.match? e.name}
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(project_to_compare)
+    self.name() == project_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    name = project.fetch("name")
+    id = project.fetch("id").to_i
+    Project.new({:name => name, :id => id})
+  end
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE projects SET name = '#{@name}' WHERE id = #{@id}")
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE projects_id = #{@id};")
+  end
+
+  
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,6 +1,6 @@
-class Song
+class Volunteer
   attr_reader :id
-  attr_accessor :name, :album_id
+  attr_accessor :name, :project_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
@@ -8,5 +8,69 @@ class Song
     @id = attributes.fetch(:id)
   end
 
-  
+  def self.all
+    returned_volunteers = DB.exec('SELECT * FROM volunteers;')
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch('name')
+      id = volunteer.fetch('id').to_i
+      project_id = volunteer.fetch('project_id').to_i
+      volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+    end
+    volunteers
+  end
+
+  def self.search(x)
+    volunteers = Volunteer.all
+    volunteers.select {|e| /#{x}/i.match? e.name}
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}'), ('#{@project_id}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(volunteer_to_compare)
+    self.name() == volunteer_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM volunteers *;")
+  end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch("name")
+    id = volunteer.fetch("id").to_i
+    Volunteer.new({:name => name, :id => id})
+  end
+
+  def self.find_by_project(id)
+    results = DB.exec("SELECT * FROM projects_volunteers WHERE project_id = #{id};")
+    volunteers = []
+    results.each {|result|
+      volunteer_id = result.fetch('volunteer_id')
+      volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{volunteer_id};").first
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :id => id}))
+    }
+    volunteers
+  end
+
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id}")
+  end
+
+  def delete
+    DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};")
+  end
+
+  def self.sort()
+    volunteers = Volunteer.all
+    volunteers.sort { |a, b| a.name <=> b.name }
+  end
 end
